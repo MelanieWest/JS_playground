ceb71a74e15105be59fdc50de969b8339e890fe6
Melanie West <dhmofan@gmail.com>
2017-12-18 15:06:51 -0500

    add error handlers and conditional  blocks for grouping.  Not yet functional.  some errors were introduced:  now get 'NaN' for a result instead of a value.  This is not a big concern since the new code is in a separate file from the original code, but it needs to be traced for moving forward

--- calc_new.js
+++ calc_new.js
@@ -20,47 +20,122 @@ $(document).ready(function(){
         var b = []; //for use with parens at second data value
         var opx = []; //for use with parens in first value
         var opy = []; //for use with parens in second value
+        var xTemp='', newx='', yTemp='', newy='';
         
+        var paren = false;
         var opSel = false;
         var eqSel = false;
         var xSt='', ySt='', xInt=0, yInt=0, Result=0;
         var op = '', opVal = '';
     
     
-            $(".number").on("click", function() {
-            if (opSel == false) {  // only append first number if no operator has been pressed
+        $(".number").on("click", function() {
+          if(paren==true){      //if open parens, prep number-strings for stuffing into arrays for later eval
+              if(opSel ==false){    //parenthetical expression will resolve to the first value
+                xTemp = $(this).val();
+                newx += xTemp;
+                $("#first-number").append(newx);
+            }
+              else{             // parenthetical expression will resolve to second value
+                yTemp = $(this).val();
+                newy += yTemp;
+                $("#second-number").append(newy);
+            }
+          }
+
+          if(paren == false){     //if no parens are chosen (or they are closed),
+            // proceed with a simple number entry (no arrays)
+            //note:  insert error handler here to prevent number entry imm. after closed parens
+
+            if (opSel == false) {  // only concat first number (string) if no operator has been pressed
                 var x= $(this).val();
-                xSt += x;
+                xSt += x;               //string for now;  parse to a number when evaluating
                 $("#first-number").html(xSt);
                 console.log(xSt);
             }
             else {
-                var y= $(this).val();
-                ySt += y;
+                var y= $(this).val();       //concat second number (string) if operator is selected
+                ySt += y;               //string for now;  parse to a number when evaluating
                 $("#second-number").html(ySt);
                 console.log(ySt);
-            }
-          });
-    
+            }                           
+          }     //end of false parens block
+
+        });
+
     
     //display the operator
     
           $(".operator").on("click", function() {
-            if(xSt===''){
-                alert("You must enter a number first");
+
+            op= $(this).text();     // use this as the display image
+            opVal = $(this).val();    //use the value for decision-making
+
+            if(paren==true){   //this will stuff arrays until parens are closed
+                if(newx==''){       //error check  - no number entered before operator
+                    alert("You must enter a number first");                    
+                }
+                else if (opSel==false){  //stuffs arrays that resolve to first value
+                   if(newx ==''){
+                       alert('you must first enter a number');
+                   }
+                   else{
+                       a.push(newx);    //push the numeric value into the numbers array;
+                       opx.push(opVal); //push the operation selected into operators array
+                       newx ='';        //reset for next entry
+                       $("#first-number").append(newx+op);
+                   }
+                }
+                else if(opSel==true){ //stuffs arrays that resolve to second value
+                    if(newy ==''){
+                        alert('you must first enter a number');
+                    }
+                    else{
+                        b.push(newy);    //push the numeric value into the numbers array;
+                        opy.push(opVal); //push the operation selected into operators array
+                        newy ='';        //reset for next entry
+                        $("#second-number").append(newy+op);
+                    }
+                }
+
             }
-            else{
-                op= $(this).text();
-                opVal = $(this).val();
-                console.log(opVal);
-                $("#operator").html("<h1>"+ op +" </h1> ");
-                opSel = true;    
+            if(paren==false){  // this sets the operation between the two resolved values
+                if(xSt=''){     //if the first value doesn't exist
+                    alert("You must enter a number first");                    
+                }
+                else{           // if the first value does exist
+                    $("#operator").html("<h1>"+ op +" </h1> ");     //display the operator (text)
+                    opSel = true;       //this tells calc that entries after this apply to second value        
+                }
             }
-     //       console.log(op)
+
           });
 
     $(".grouping").on("click",function(){
-        alert("You chose parentheses!!");
+
+        p= $(this).text();          //image for display
+        pVal = $(this).val();       //value for decision-making
+        if(pVal=="left-paren"){     //begin a sequence of operations in parens
+            paren = true;
+            if(opSel==false){
+                $("#first-number").append('(');                
+            }
+            else{
+                $("#second-number").append('(');                
+            }
+            
+        }
+        else{                   //end the sequence of operations
+            paren = false;
+            if(opSel==false){
+                $("#first-number").append(')');                
+            }
+            else{
+                $("#second-number").append(')');                
+            }
+
+        }
+        alert('you chose '+p+', and boolean for parens is now '+paren);
     })
     
     //execute the operations when 'equal' is pressed;
@@ -103,6 +178,7 @@ $(document).ready(function(){
     
     $(".clear").on('click', function() {
                     xSt = ySt = "";
+                    pareb = false;
                     opSel = false;
                     eqSel = false;
                     $("#first-number, #second-number, #operator, #result,#xVal,#yVal,#opEcho",).html("");
@@ -118,6 +194,7 @@ $(document).ready(function(){
 
     function clear(){
         xSt = ySt = "";
+        paren = false;
         opSel = false;
         eqSel = false;
         $("#first-number, #second-number, #operator, #result,#xVal,#yVal,#opEcho").html(""); 
